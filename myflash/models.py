82d88bc72b34a449463f1ea7dc6ee4c45c78082a
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-28 00:21:38 +0300

    add instance for delete, save and update on models

--- myflash/models.py
+++ myflash/models.py
@@ -20,3 +20,26 @@ class FlashCards(models.Model):
     @classmethod
     def update_flash_cards(cls, id, value):
         cls.objects.filter(id=id).update(name = value)
+
+
+
+    
+    
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+
+    @classmethod
+    def update_category(cls, id, value):
+        cls.objects.filter(id=id).update(name = value)
