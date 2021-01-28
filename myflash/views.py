982299ac757f1cc20670fd51ef36972a3d841e73
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-29 01:44:15 +0300

    add search view function

--- myflash/views.py
+++ myflash/views.py
@@ -43,3 +43,16 @@ def loginPage(request):
 def logoutUser(request):
     logout(request)
     return redirect('login')
+
+
+def search_cards(request):
+    if 'cards' in request.GET and request.GET["cards"]:
+        cards = request.GET.get("cards")
+        searched_images = Image.filter_by_location(cards)
+        message = f"{cards}"
+        print("Image.......",searched_images)
+        return render(request, 'cards.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'cards.html', {"message": message})
+
