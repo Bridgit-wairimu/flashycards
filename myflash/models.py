fd338d0915c95e8eb462098d32d46d8ccf3bb259
Bridgit-wairimu <kirikabetty@gmail.com>
2021-01-29 11:16:53 +0300

    imported the auth.models import user and reverse

--- myflash/models.py
+++ myflash/models.py
@@ -1,15 +1,22 @@
-
 from django.db import models
 import datetime as dt
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
+from django.urls import reverse
+from django.db.models.signals import post_save
+
+
 
 # Create your models here.
 class FlashCards(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
     name = models.CharField(max_length =30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="flashcards", blank=True,null=True)
     description = models.TextField(max_length =200, blank=True, null=True)
     pub_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     category = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='media/', blank=True, null=True, default='default.jpg')
     images = CloudinaryField('images')
     def __str__(self):
         return str(self.name)
@@ -25,4 +32,9 @@ class FlashCards(models.Model):
     @classmethod
     def update_flash_cards(cls, id, value):
         cls.objects.filter(id=id).update(name = value)
+        
+    def get_absolute_url(self):
+        return reverse('index')
+
+
 
